6c70360b22f13b44988a497163476409ad353ab8
Roman <mazaxaka.tyt@gmail.com>
2022-02-14 19:50:55 +0200

    add ProductsListItem like class component

--- src/components/Products/ProductsListItem.js
+++ src/components/Products/ProductsListItem.js
@@ -5,49 +5,61 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
-import React from 'react'
+import React, { Component } from 'react'
 import './ProductsListItem.css'
 import PropTypes from 'prop-types'
 
-export const ProductsListItem = ({
-    image,
-    name,
-    description,
-    type,
-    capacity,
-    price,
-}) => {
-    return (
-        <>
-            <Card>
-                <CardContent>
-                    <div className="product-img">
-                        <img src={image} alt="iphone"></img>
-                    </div>
-                    <h4>{name}</h4>
-                    <p>{description}</p>
-                    <div className="product-features">Type: {type}</div>
-                    <div className="product-features">
-                        Capacity: {capacity} Gb
-                    </div>
-                    <div className="product-price">{price} $</div>
-                    <div className="product-quantity">
-                        <Button variant="contained">-</Button>
-                        <TextField
-                            size="small"
-                            value="1"
-                            className="text-center"
-                        />
-                        <Button variant="contained">+</Button>
-                    </div>
-                </CardContent>
-                <CardActions className="wrap-btn-add-to-cart">
-                    <Button variant="outlined">Add to cart</Button>
-                </CardActions>
-            </Card>
-        </>
-    )
+export class ProductsListItem extends Component {
+    render() {
+        return (
+            <>
+                <Card>
+                    <CardContent>
+                        <div className="product-img">
+                            <img src={this.props.image} alt="iphone"></img>
+                        </div>
+                        <h4>{this.props.name}</h4>
+                        <p>{this.props.description}</p>
+                        <div className="product-features">
+                            Type: {this.props.type}
+                        </div>
+                        <div className="product-features">
+                            Capacity: {this.props.capacity} Gb
+                        </div>
+                        <div className="product-price">
+                            {this.props.price} $
+                        </div>
+                        <div className="product-quantity">
+                            <Button variant="contained">-</Button>
+                            <TextField
+                                size="small"
+                                value="1"
+                                className="text-center"
+                            />
+                            <Button variant="contained">+</Button>
+                        </div>
+                    </CardContent>
+                    <CardActions className="wrap-btn-add-to-cart">
+                        <Button variant="outlined">Add to cart</Button>
+                    </CardActions>
+                </Card>
+            </>
+        )
+    }
 }
+
+// export const ProductsListItem = ({
+//     image,
+//     name,
+//     description,
+//     type,
+//     capacity,
+//     price,
+// }) => {
+//     return (
+//     )
+// }
+
 ProductsListItem.propTypes = {
     image: PropTypes.string,
     name: PropTypes.string.isRequired,
